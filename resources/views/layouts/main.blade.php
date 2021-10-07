39e0b4c783d58368796bce62162134ce68543740
Harun Ar - Rasyid <32662500+Harun1804@users.noreply.github.com>
2021-10-08 06:15:21 +0700

    Ubah Desain Login

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>Penomoran Surat dan Memo</title>
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-    <link rel="icon" href="{{ asset('img/icon.ico') }}" type="image/x-icon" />
+    <link rel="icon" href="{{ asset('img/logo.ico') }}" type="image/x-icon" />
 
     <!-- Fonts and icons -->
     <script src="{{ asset('assets/js/plugin/webfont/webfont.min.js') }}"></script>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,89 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<title>Penomoran Surat dan Memo</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="{{ asset('img/icon.ico') }}"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/vendor/bootstrap/css/bootstrap.min.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/vendor/animate/animate.css') }}">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/vendor/css-hamburgers/hamburgers.min.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/vendor/animsition/css/animsition.min.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/vendor/select2/select2.min.css') }}">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/vendor/daterangepicker/daterangepicker.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/css/util.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/auth/css/main.css') }}">
-<!--===============================================================================================-->
+    <title>Penomoran Surat dan Memo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="{{ asset('img/logo.ico') }}" />
+    <!--===============================================================================================-->
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+    <style>
+		body {
+			background-size: 100% 100%;
+			background-repeat: no-repeat;
+		}
+
+		body,html {
+			background-image: url('{{ asset('img/bg-01.jpg') }}');
+			height: 100%;
+		}
+
+		#profile-img {
+			height:180px;
+		}
+		.h-80 {
+			height: 80% !important;
+		}
+    </style>
+
 </head>
+
 <body>
-	
-	<div class="limiter">
-		<div class="container-login100">
-			<div class="wrap-login100">
-				@if (session('failed'))
+	<div class="container h-80">
+		<div class="row align-items-center h-100">
+			<div class="col-3 mx-auto">
+				<div class="text-center">
+					@if (session('failed'))
 					<div class="alert alert-danger">{{ session('failed') }}</div>
-				@endif
-				@if (session('success'))
+					@endif
+					@if (session('success'))
 					<div class="alert alert-success">{{ session('success') }}</div>
-				@endif
-				<div class="login100-form-title" style="background-image: url({{ asset('img/bg-01.jpg') }});">
-					<span class="login100-form-title-1">
-						Sign In
-					</span>
+					@endif
+					<p id="profile-name" class="profile-name-card"></p>
+					<form class="form-signin" action="{{ route('verify.login') }}" method="post">
+						@csrf
+						
+						<input type="text" name="username" id="inputPassword" class="form-control form-group" placeholder="Username" required autofocus>
+						<input type="password" name="password" id="inputPassword" class="form-control form-group" placeholder="password" required autofocus>
+						<button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Login</button>
+					</form>
 				</div>
-
-				<form class="login100-form validate-form" action="{{ route('verify.login') }}" method="post">
-					@csrf
-					<div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
-						<span class="label-input100">Username</span>
-						<input class="input100" type="text" name="username" placeholder="Enter username" name="username">
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input m-b-18" data-validate = "Password is required">
-						<span class="label-input100">Password</span>
-						<input class="input100" type="password" name="password" placeholder="Enter password" name="password">
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn" type="submit">
-							Login
-						</button>
-					</div>
-				</form>
 			</div>
 		</div>
 	</div>
-	
-<!--===============================================================================================-->
-	<script src="{{ asset('assets/auth/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('assets/auth/vendor/animsition/js/animsition.min.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('assets/auth/vendor/bootstrap/js/popper.js') }}"></script>
-	<script src="{{ asset('assets/auth/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('assets/auth/vendor/select2/select2.min.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('assets/auth/vendor/daterangepicker/moment.min.js') }}"></script>
-	<script src="{{ asset('assets/auth/vendor/daterangepicker/daterangepicker.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('assets/auth/vendor/countdowntime/countdowntime.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('assets/auth/js/main.js') }}"></script>
+
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 </body>
+
 </html>
